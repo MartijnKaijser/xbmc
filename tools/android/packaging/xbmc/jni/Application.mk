c5f60b831a6a77bd4159e0b0cabda70c82f2d6a1
Martijn Kaijser <mcm.kaijser@gmail.com>
2016-10-01 00:57:31 +0200

    test

--- tools/android/packaging/xbmc/jni/Application.mk
+++ tools/android/packaging/xbmc/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_PLATFORM := android-9
 APP_OPTIM = debug
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a arm64-v8a x86
 # This would support both ARVMv5TE and ARMv7
-#APP_ABI := armeabi armeabi-v7a
+#APP_ABI := armeabi armeabi-v7a arm64-v8a x86
